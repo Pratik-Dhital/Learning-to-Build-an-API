11a9b4378c05605bc25726b13f4ad07ebfd98252
Pratik-Dhital <pratikdhital2060@gmail.com>
2026-05-25 12:58:32 +0545

    learned about the drf decorators and used them

--- soda/views.py
+++ soda/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 from .models import Drink
 from django.http import JsonResponse, request
 from .serializers import DrinksSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 
+
+@api_view(['GET', 'POST'])
 def drink_list(request):
-    drinks = Drink.objects.all()
-    serializer = DrinksSerializer(drinks, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == 'GET':
+        drinks = Drink.objects.all()
+        serializer = DrinksSerializer(drinks, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    if request.method == 'POST':
+        serializer = DrinksSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
